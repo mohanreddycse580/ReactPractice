9bb96a34886eb9c6382d3a4d6b98ef03913b4143
Mohan Reddy <mohanreddycse580@gmail.com>
2018-12-21 07:55:26 +0530

    12-21-2018 Morning Practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,8 @@ state = {
 { name :'mohan' ,age :'30'},
 { name :'Ramesh', age :'35'},
 { name : 'Reddy', age :'40'}
-  ]
+  ],
+  togglepersons:false
 }
 changePersionData=()=>{
   this.setState(
@@ -28,14 +29,35 @@ changedTextValue=(event)=>{
     ]}
   )
 }
+togglepersonhandler=()=>{
+  const doesShow=this.state.togglepersons;
+  this.setState({
+    togglepersons:!doesShow
+  
+  })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <h1>Welcome  To ReactJS!</h1>
-        <button onClick={this.changePersionData}>Change Persion</button>
+
+    let persondata=null;
+    {
+      persondata= (this.state.togglepersons===true?
+      <div className="PersonClass">
+
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person click={this.changePersionData}  changed={this.changedTextValue} name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
+      </div>:null
+      );
+
+    }
+
+    return (
+      <div className="App">
+        <h1>Welcome  To ReactJS!</h1>
+        <button onClick={this.togglepersonhandler}>Show Persion</button>
+        {persondata}
       </div>
     );
   }
